b5ca2af65d71d4015520936c757ad66c14ba0e0c
0tak2 <0tak2.code@gmail.com>
2025-02-10 13:13:28 +0900

    feat: add common ui aseets

---
+++ PicplzClient/PicplzClient/Presentation/Common/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  Fonts.swift
+//  PicplzClient
+//
+//  Created by 임영택 on 2/9/25.
+//
+
+import UIKit
+
+struct Fonts {
+    static let titleBig = UIFont.systemFont(ofSize: 24, weight: .bold)
+    static let buttonTitle = UIFont.systemFont(ofSize: 16, weight: .bold)
+}
