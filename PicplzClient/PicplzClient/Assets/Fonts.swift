c03b698547df7f446bb7939abaa09adec84d6b3c
0tak2 <0tak2.code@gmail.com>
2025-02-18 15:42:49 +0900

    assets: 폰트 크기 잘못 설정되어 있었던 부분 수정

--- PicplzClient/PicplzClient/Assets/Fonts.swift
+++ PicplzClient/PicplzClient/Assets/Fonts.swift
@@ -28,8 +28,8 @@ extension UIFont {
     
     static let body = UIFont(name: FontFamily.pretendardRegular.rawValue, size: 14)!
     static let bigBody = UIFont(name: FontFamily.pretendardRegular.rawValue, size: 16)!
-    static let buttonTitle = UIFont(name: FontFamily.pretendardBold.rawValue, size: 18)!
-    static let title = UIFont(name: FontFamily.pretendardBold.rawValue, size: 24)!
+    static let buttonTitle = UIFont(name: FontFamily.pretendardBold.rawValue, size: 16)!
+    static let title = UIFont(name: FontFamily.pretendardBold.rawValue, size: 20)!
     static let bigTitle = UIFont(name: FontFamily.pretendardBold.rawValue, size: 24)!
     static let caption = UIFont(name: FontFamily.pretendardRegular.rawValue, size: 12)!
 }
