c5290a5236de5ac94b5779dc11f222beaf979bd8
0tak2 <0tak2.code@gmail.com>
2025-02-20 16:14:47 +0900

    feat: SignUpSession에 경력 타입 추가

--- PicplzClient/PicplzClient/Presentation/Login/Model/SignUpSession.swift
+++ PicplzClient/PicplzClient/Presentation/Login/Model/SignUpSession.swift
@@ -11,6 +11,7 @@ final class SignUpSession: CustomStringConvertible {
     var nickname: String = ""
     var profileImageUrl: URL?
     var memberType: MemberType?
+    var photoCareerType: CareerType?
     var photoCareerYears: Int?
     var photoCareerMonths: Int?
     var photoSpecializedThemes: [String]?
@@ -19,6 +20,7 @@ final class SignUpSession: CustomStringConvertible {
         "SignUpSession: nickname: \(nickname) "
         + "/ profileImageUrl: \(String(describing: profileImageUrl)) "
         + "/ memberType: \(String(describing: memberType)) "
+        + "/ photoCareerType: \(String(describing: photoCareerType)) "
         + "/ photoCareerYears: \(String(describing: photoCareerYears)) "
         + "/ photoCareerMonths: \(String(describing: photoCareerMonths)) "
         + "/ photoSpecializedThemes: \(String(describing: photoSpecializedThemes))"
@@ -27,5 +29,11 @@ final class SignUpSession: CustomStringConvertible {
     enum MemberType {
         case customer
         case photographer
+    }
+    
+    enum CareerType {
+        case major // 사진 전공
+        case job // 수익 창출
+        case influencer // SNS 계정 운영
     }
 }
