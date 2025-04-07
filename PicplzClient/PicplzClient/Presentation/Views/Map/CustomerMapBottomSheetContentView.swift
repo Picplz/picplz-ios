b9ff58aa05ee9f2472d8590b89197788a86b3bfc
0tak2 <0tak2.code@gmail.com>
2025-04-07 21:41:51 +0900

    feat: 정렬 방법 선택 픽커 추가

--- PicplzClient/PicplzClient/Presentation/Views/Map/CustomerMapBottomSheetContentView.swift
+++ PicplzClient/PicplzClient/Presentation/Views/Map/CustomerMapBottomSheetContentView.swift
@@ -13,8 +13,9 @@ class CustomerMapBottomSheetContentView: UIView {
     let topMargin: CGFloat = 28.0
     
     private var collectionView: UICollectionView!
-    private var dataSource: UICollectionViewDiffableDataSource<HeaderSection, MapListFilter>!
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
+    // FIXME: should be injected from outside
     private var photographerFilters: [MapListFilter] = MapListFilter.photographerFilters
     private var hashTagFilters: [MapListFilter] = MapListFilter.hashTagFilters
     
@@ -25,20 +26,31 @@ class CustomerMapBottomSheetContentView: UIView {
         
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init())
         collectionView.collectionViewLayout = getLayout()
-        collectionView.delegate = self
         
         collectionView.register(FilterItemCell.self, forCellWithReuseIdentifier: "FilterItemCell")
+        collectionView.register(OrderItemCell.self, forCellWithReuseIdentifier: "OrderItemCell")
         
-        dataSource = UICollectionViewDiffableDataSource<HeaderSection, MapListFilter>(collectionView: collectionView) { collectionView, indexPath, item in
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilterItemCell", for: indexPath) as? FilterItemCell else {
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) { collectionView, indexPath, item in
+            switch item {
+            case .filter(let filter):
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilterItemCell", for: indexPath) as? FilterItemCell {
+                    cell.configure(filter: filter) { [weak self] filter in
+                        self?.didFilterButtonTapped(filter: filter)
+                    }
+                    return cell
+                }
+            case .order:
+                if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OrderItemCell", for: indexPath) as? OrderItemCell {
+                    cell.configure { [weak self] orderBy in
+                        self?.didOrderBySelected(order: orderBy)
+                    }
+                    return cell
+                }
+            default:
                 return nil
             }
             
-            cell.configure(filter: item) { [weak self] filter in
-                self?.didFilterButtonTapped(filter: filter)
-            }
-            
-            return cell
+            return nil
         }
         
         applyDatasource()
@@ -75,11 +87,11 @@ class CustomerMapBottomSheetContentView: UIView {
     }
     
     func applyDatasource() {
-        var snapshot = NSDiffableDataSourceSnapshot<HeaderSection, MapListFilter>()
-        snapshot.appendSections([.photographerFilter])
-        snapshot.appendItems(photographerFilters)
-        snapshot.appendSections([.hashTagFilter])
-        snapshot.appendItems(hashTagFilters)
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.photographerFilter, .hashTagFilter, .orderControl])
+        snapshot.appendItems(photographerFilters.map({ Item.filter($0) }), toSection: .photographerFilter)
+        snapshot.appendItems(hashTagFilters.map({ Item.filter($0) }), toSection: .hashTagFilter)
+        snapshot.appendItems([.order], toSection: .orderControl)
         dataSource.apply(snapshot)
     }
     
@@ -89,18 +101,25 @@ class CustomerMapBottomSheetContentView: UIView {
         print(filter)
     }
     
-    enum HeaderSection {
-        case photographerFilter
-        case hashTagFilter
+    private func didOrderBySelected(order: OrderBy) {
+        // TODO: Connect to viewModel
+        print(order)
     }
 }
 
-extension CustomerMapBottomSheetContentView: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath)
-    }
+fileprivate enum Section {
+    case photographerFilter
+    case hashTagFilter
+    case orderControl
 }
 
+fileprivate enum Item: Hashable {
+    case filter(MapListFilter)
+    case order
+    case photographerList
+}
+
+// FIXME: separate file
 fileprivate final class FilterItemCell: UICollectionViewCell {
     private var filter: MapListFilter?
     private(set) var button: UIButton?
@@ -142,6 +161,94 @@ fileprivate final class FilterItemCell: UICollectionViewCell {
     }
 }
 
+// FIXME: separate file
+fileprivate final class OrderItemCell: UICollectionViewCell, UIPickerViewDelegate, UIPickerViewDataSource {
+    private let orderRules: [OrderBy] = OrderBy.allCases
+    private var selectedRule = OrderBy.distance
+    private let textField = UITextField()
+    private let pickerView = UIPickerView()
+    private let symbolImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "DropDownSymbol")
+        return imageView
+    }()
+    private var didSelectHandler: ((_ orderBy: OrderBy) -> Void)?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // MARK: set pickerView
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        // MARK: set pickerView as textField's inputView
+        textField.inputView = pickerView
+        textField.tintColor = .clear // 커서 숨기기
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(cellTapped))
+        tapGesture.cancelsTouchesInView = false
+        addGestureRecognizer(tapGesture)
+        
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        textField.text = selectedRule.title
+        textField.font = UIFont.caption
+        textField.textColor = .grey5
+        
+        addSubview(textField)
+        textField.snp.makeConstraints { make in
+            make.leading.equalTo(self.snp.leading)
+            make.verticalEdges.equalTo(self)
+        }
+        
+        addSubview(symbolImageView)
+        symbolImageView.snp.makeConstraints { make in
+            make.leading.equalTo(textField.snp.trailing).offset(4)
+            make.trailing.equalTo(self.snp.trailing)
+            make.centerY.equalTo(textField.snp.centerY)
+        }
+    }
+    
+    func configure(didSelectHandler: ((_ orderBy: OrderBy) -> Void)?) {
+        self.didSelectHandler = didSelectHandler
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        orderRules.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        orderRules[row].title
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerViewSelected()
+    }
+    
+    private func pickerViewSelected() {
+        let row = pickerView.selectedRow(inComponent: 0)
+        selectedRule = orderRules[row]
+        textField.text = selectedRule.title
+        textField.resignFirstResponder()
+        
+        didSelectHandler?(selectedRule)
+    }
+    
+    @objc private func cellTapped() {
+        textField.becomeFirstResponder()
+    }
+}
+
 struct MapListFilter: Hashable {
     let filterTitle: String
     let image: UIImage?
@@ -167,6 +274,20 @@ struct MapListFilter: Hashable {
     enum FilterType {
         case photographerFilter
         case hashTagFilter
+    }
+}
+
+fileprivate enum OrderBy: CaseIterable {
+    case distance
+    case popularity
+    
+    var title: String {
+        switch self {
+        case .distance:
+            return "거리순"
+        case .popularity:
+            return "인기순"
+        }
     }
 }
 
