e4225cee580a8caeff5cb1efe1a256091e6dabd2
0tak2 <0tak2.code@gmail.com>
2025-09-02 00:17:01 +0900

    chore: SignUpSession에 주석 추가. 삭제 필요한 프로퍼티는 TODO 주석으로 명시

--- PicplzClient/PicplzClient/Domain/Member/Entity/SignUpSession.swift
+++ PicplzClient/PicplzClient/Domain/Member/Entity/SignUpSession.swift
@@ -7,13 +7,30 @@
 
 import Foundation
 
+/// 회원가입 세션
 final class SignUpSession: CustomStringConvertible {
+    /// 닉네임
     var nickname: String = ""
+    
+    /// 프로필 이미지 업로드 후 URL
     var profileImageUrl: URL?
+    
+    /// 회원 종류
     var memberType: MemberType?
+    
+    /// 경험 종류 (삭제 필요)
+    // TODO: 삭제 필요
     var photoCareerType: CareerType?
+    
+    /// 경험 기간 - 연도
+    // TODO: 삭제 필요
     var photoCareerYears: Int?
+    
+    /// 경험 기간 - 개월 수 (삭제 필요)
+    // TODO: 삭제 필요
     var photoCareerMonths: Int?
+    
+    /// 자신 있는 분위기
     var photoSpecializedThemes: [String]?
     
     var description: String {
@@ -31,6 +48,7 @@ final class SignUpSession: CustomStringConvertible {
         case photographer
     }
     
+    // TODO: 삭제 필요
     enum CareerType {
         case major // 사진 전공
         case job // 수익 창출
