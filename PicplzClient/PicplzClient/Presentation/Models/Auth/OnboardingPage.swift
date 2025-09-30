b601259feb06b2c68a81b343f5f9e8107ddbc468
임영택 <0tak2.code@gmail.com>
2025-09-30 11:24:36 +0900

    feat: 온보딩 인트로 이미지 적용 (#53)

--- PicplzClient/PicplzClient/Presentation/Models/Auth/OnboardingPage.swift
+++ PicplzClient/PicplzClient/Presentation/Models/Auth/OnboardingPage.swift
@@ -6,14 +6,15 @@
 //
 
 import Foundation
+import UIKit
 
 struct OnboardingPage: Hashable {
     let onboardingMessage: String
-    let onboardingImageName: String
+    let onboardingImage: ImageResource?
 
     static let pages: [OnboardingPage] = [
-        .init(onboardingMessage: "내 인생샷 찍어줄\n픽플과 위치기반 매칭!", onboardingImageName: ""),
-        .init(onboardingMessage: "인생샷 맛집\n핫플레이스 추천", onboardingImageName: ""),
-        .init(onboardingMessage: "나의 인생 프사,\n이젠 픽플즈가 함께", onboardingImageName: "")
+        .init(onboardingMessage: "내 인생샷 찍어줄\n픽플과 위치기반 매칭!", onboardingImage: .intro1),
+        .init(onboardingMessage: "인생샷 맛집\n핫플레이스 추천", onboardingImage: .intro2),
+        .init(onboardingMessage: "나의 인생 프사,\n이젠 픽플즈가 함께", onboardingImage: .intro3)
     ]
 }
