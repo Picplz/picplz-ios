aea97fd433d336b9b18fb3db99cedec1205e6f32
0tak2 <0tak2.code@gmail.com>
2026-01-19 20:00:37 +0900

    [Chore] Presentation 모듈 Settings 추가
    Generate Swift Asset Symbol Extensions = YES

--- Projects/Presentation/Project.swift
+++ Projects/Presentation/Project.swift
@@ -14,6 +14,15 @@ let project = Project.create(
   infoPlist: .default,
   hasResources: true,
   dependencies: [.domain],
+  settings: .settings(
+    base: [
+      "ASSETCATALOG_COMPILER_GENERATE_SWIFT_ASSET_SYMBOL_EXTENSIONS": "YES" // Generate Swift Asset Symbol Extensions = YES
+    ],
+    configurations: [
+      .debug(name: "Debug"),
+      .release(name: "Release"),
+    ]
+  ),
   externalPackages: [.composableArchitecture],
   name: "Presentation"
 )
