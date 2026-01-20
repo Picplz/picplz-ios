16fb50a8a4b98e9e5fb49f22eeacf96e0c3e99c7
0tak2 <0tak2.code@gmail.com>
2026-01-20 14:30:06 +0900

    [Feat] rename `Network` module to `Networking`

--- Projects/PicplzApp/Project.swift
+++ Projects/PicplzApp/Project.swift
@@ -20,6 +20,6 @@ let project = Project.create(
     ]
   ),
   hasResources: false,
-  dependencies: [.features, .domain, .network, .storage, .sharedSupports],
+  dependencies: [.features, .domain, .networking, .storage, .sharedSupports],
   name: "PicplzApp"
 )
