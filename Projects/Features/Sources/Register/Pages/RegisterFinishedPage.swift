cfae5d02ca688fe7e69a264d2a62d02bcaf51f48
0tak2 <0tak2.code@gmail.com>
2026-03-03 13:07:20 +0900

    [Feat] 회원가입 - 완료 페이지

---
+++ Projects/Features/Sources/Register/Pages/RegisterFinishedPage.swift
@@ -0,0 +1,48 @@
+//
+//  RegisterFinishedPage.swift
+//  Features
+//
+//  Created by 임영택 on 3/2/26.
+//
+
+import Common
+import Domain
+import PhotosUI
+import SwiftUI
+
+struct RegisterFinishedPage: View {
+  let userNickname: String
+
+  // MARK: - Spacings
+  let titleTopSpacing: CGFloat = 66
+  let titleBottomSpacing: CGFloat = 30
+
+  var body: some View {
+    VStack(spacing: 0) {
+      Text("안녕하세요 \(userNickname)님!")
+        .typo(.pTitle)
+        .frame(maxWidth: .infinity, alignment: .center)
+        .padding(.top, titleTopSpacing)
+        .padding(.bottom, titleBottomSpacing)
+
+      Image(.registrationCompleted)
+      .padding(.bottom, 70)
+
+      Text("가입을 축하드려요.\n함께 사진 촬영하러 가볼까요?")
+        .typo(.pTitle)
+        .multilineTextAlignment(.center)
+        .frame(maxWidth: .infinity, alignment: .center)
+
+      Spacer()
+
+      Button1(title: "시작하기") {
+        //
+      }
+    }
+    .padding(.horizontal)
+  }
+}
+
+#Preview {
+  RegisterFinishedPage(userNickname: "유가영")
+}
