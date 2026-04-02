b7ca75dfa8867b828399bd22eb6f8286ed3a06d4
_GGDol <bohg6799@naver.com>
2026-04-02 18:36:11 +0900

    [Feat] 지난 촬영 내역 카드 UI 및 모델 구현 (#30)
    - PastShooting 모델 및 ShootingStatus(완료/취소) 정의
    - PastShootingCardView 카드 컴포넌트 구현
    - 촬영 취소 시 가격 취소선, 채팅 가기 버튼만 표시
    - 촬영 완료 시 채팅 가기 + 리뷰 쓰기 버튼 표시

--- Projects/Features/Sources/MyPage/PastShootings/PastShootingsFeature.swift
+++ Projects/Features/Sources/MyPage/PastShootings/PastShootingsFeature.swift
@@ -10,13 +10,35 @@ import Foundation
 
 @Reducer
 public struct PastShootingsFeature {
+    // MARK: - Placeholder Model (추후 Domain 모델로 교체)
+    public struct PastShooting: Equatable, Identifiable, Hashable {
+        public let id: String
+        public let photographerName: String
+        public let photographerImageURL: String?
+        public let title: String                // 촬영명
+        public let price: Int                   // 가격
+        public let status: ShootingStatus       // 촬영 상태
+        public let dateTime: String             // 촬영 일시
+        public let location: String             // 촬영 장소
+        public let paymentDate: String          // 결제 날짜
+    }
+
+    public enum ShootingStatus: String, Equatable, Hashable {
+        case completed = "촬영 완료"
+        case cancelled = "촬영 취소"
+    }
+
     @ObservableState
     public struct State: Equatable, Hashable {
+        var pastShootings: [PastShooting] = []
         public init() {}
     }
 
     public enum Action: Hashable {
         case backButtonTapped
+        case chatButtonTapped(String)           // TODO: 채팅 화면 연동
+        case reviewButtonTapped(String)         // TODO: 리뷰 작성 화면 연동
+        case orderDetailTapped(String)          // TODO: 주문 상세 화면 연동
     }
 
     public init() {}
@@ -25,6 +47,15 @@ public struct PastShootingsFeature {
         Reduce { state, action in
             switch action {
             case .backButtonTapped:
+                return .none
+            case .chatButtonTapped:
+                // TODO: 채팅 화면 연동
+                return .none
+            case .reviewButtonTapped:
+                // TODO: 리뷰 작성 화면 연동
+                return .none
+            case .orderDetailTapped:
+                // TODO: 주문 상세 화면 연동
                 return .none
             }
         }
